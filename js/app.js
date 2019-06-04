9e62ef331812244ff0893261eb5dbc2158b0c527
Marius De Allie <spectre4300s@gmail.com>
2019-06-04 12:49:08 -0400

    Create click event listener on catImage element

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 const catImage = document.querySelector('.cat-image');
 const counter = document.querySelector('.click-count');
 let count = 0;
+
+// Click event listener on catImage 
+catImage.addEventListener('click', function() {
+
+});
