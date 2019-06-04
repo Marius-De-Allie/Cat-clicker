052b16e4e51d5d5da84832b3d62f0d82e2a5199b
Marius De Allie <spectre4300s@gmail.com>
2019-06-04 16:19:53 -0400

    Add logic to click event listener function to increment count
    Add logic to image click event listener to increment value of 'count'
    variable for each click

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const catImage = document.querySelector('.cat-image');
 const counter = document.querySelector('.click-count');
 let count = 0;
 
-// Click event listener on catImage 
+// Click event listener on catImage
 catImage.addEventListener('click', function() {
-
+  count++;
 });
