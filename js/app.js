38db021f67b27a675bb5b62b0f6c85ac93f5be60
Marius De Allie <spectre4300s@gmail.com>
2019-06-05 11:22:43 -0400

    Create new p element and assign to variable nameElement via DOM in app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const catName1 = 'Whiskers';
-
 const catName2 = 'Leo';
+const nameElement = document.createElement('p');
 
 const catImage = document.querySelector('.cat-image');
 const counter = document.querySelector('.click-count');
