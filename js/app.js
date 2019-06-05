41dfbb6469e9c046cb51834c5353cbed6b8e7e80
Marius De Allie <spectre4300s@gmail.com>
2019-06-05 11:28:53 -0400

    Add nameElement1 to DOM as first child element of container1 via app.js
    Add nameElement1 to DOM as first child element of div element with id
    attribute of 'container1' via app.js

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ const catName2 = 'Leo';
 const nameElement1 = document.createElement('p');
 nameElement1.textContent = catName1;
 
+// Insert Cat name into DOM above cat image.
+document.querySelector('#container1').insertAdjacentElement('afterbegin',
+nameElement1);
 const catImage = document.querySelector('.cat-image');
 const counter = document.querySelector('.click-count');
 let count = 0;
