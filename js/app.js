a547634c24bc693edd4e3d406a00341f9ed75e2e
Marius De Allie <spectre4300s@gmail.com>
2019-06-05 11:39:34 -0400

    Create new p element and assign to variable nameElement2 via DOM in app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 const catName1 = 'Whiskers';
 const catName2 = 'Leo';
 const nameElement1 = document.createElement('p');
+const nameElement2 = document.createElement('p');
 nameElement1.textContent = catName1;
 
 // Insert Cat name into DOM above cat image.
+// Cat 1
 document.querySelector('#container1').insertAdjacentElement('afterbegin',
 nameElement1);
+// Cat 2
+
 const catImage = document.querySelector('.cat-image');
 const counter = document.querySelector('.click-count');
 let count = 0;
